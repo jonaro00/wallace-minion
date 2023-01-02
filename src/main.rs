92db922418999c4adfe0925746a61c714f91d3b0
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-01-03 00:42:46 +0100

    Bonk emotes

--- src/main.rs
+++ src/main.rs
@@ -435,6 +435,20 @@ async fn randomnameoff(ctx: &Context, msg: &Message) -> CommandResult {
 }
 
 const TIMEOUT_SECS: i64 = 60;
+const BONK_EMOTES: &[&str] = &[
+    "https://cdn.7tv.app/emote/631b61a98cf0978e2955b04f/2x.gif",
+    "https://cdn.7tv.app/emote/60d174a626215e098873e43e/2x.gif",
+    "https://cdn.7tv.app/emote/60b92f83db8410b2f367d817/2x.gif",
+    "https://cdn.7tv.app/emote/60aea79d4b1ea4526d9b20a9/2x.gif",
+    "https://cdn.7tv.app/emote/610d5af489f87ba0b7040211/2x.gif",
+    "https://cdn.7tv.app/emote/60afa73da3648f409ad99659/2x.gif",
+    "https://cdn.7tv.app/emote/6214fb58e82b29ebce528953/2x.gif",
+    "https://cdn.7tv.app/emote/60ecac47bb9d6a49d7d7d46e/2x.gif",
+    "https://cdn.7tv.app/emote/63444f53df8d9e6ac32ab703/2x.gif",
+    "https://cdn.7tv.app/emote/62ae151c260bc8642e64ec36/2x.gif",
+    "https://cdn.7tv.app/emote/603eacea115b55000d7282dc/2x.gif",
+    "https://cdn.7tv.app/emote/62734c1ade98b688d09661d6/2x.gif",
+];
 async fn bonk_user(ctx: &Context, msg: &Message, uid: u64) -> CommandResult {
     let gid = msg.guild_id.ok_or("Failed to get guild")?;
     gid.edit_member(ctx, UserId(uid), |m| {
@@ -458,12 +472,10 @@ async fn bonk_user(ctx: &Context, msg: &Message, uid: u64) -> CommandResult {
             ),
         )
         .await;
+    let mut rng: StdRng = SeedableRng::from_entropy();
     let _ = msg
         .channel_id
-        .say(
-            ctx,
-            "https://cdn.7tv.app/emote/631b61a98cf0978e2955b04f/2x.gif",
-        )
+        .say(ctx, BONK_EMOTES[rng.gen_range(0..BONK_EMOTES.len())])
         .await;
     Ok(())
 }
